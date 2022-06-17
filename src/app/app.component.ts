f47342a824135046f15ca58068e6a6f3f0d1c5a6
Natto-PKP <wmainvielle@gmail.com>
2022-06-17 21:29:30 +0200

    remove temp token

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,5 @@ import { AccountService } from './account.service';
 export class AppComponent {
   title = 'angular-memory';
 
-  constructor(public account: AccountService) {
-    // TEMP
-    this.account.user = true;
-    localStorage.setItem(
-      'token',
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IndtYWludmllbGxlQGdtYWlsLmNvbSIsImlkIjoyMDcwMzYsInBsYXRmb3JtIjoiaGVsb2dpIiwiaWF0IjoxNjU1NDg4ODk5LCJleHAiOjE2NTY2ODg4OTl9.zKvHvcb_fc4yHNSX6VGGEN3KGq6Fkfj7VhBszcpBdj0'
-    );
-  }
+  constructor(public account: AccountService) {}
 }
